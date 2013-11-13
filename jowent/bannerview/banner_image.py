5209457e3aac4eb6e7b5f6da710aba4cf7596b1a
Daniel Jowett <daniel@jowettenterprises.com>
2013-11-13 15:03:18 +0000

    Used paster to add BannerImage type

---
+++ jowent/bannerview/banner_image.py
@@ -0,0 +1,64 @@
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+
+
+from jowent.bannerview import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class IBannerImage(form.Schema, IImageScaleTraversable):
+    """
+    An image that will be displayed in the Banner Viewlet for it's folder, or any contained content that does not have a Lead Image set
+    """
+
+    # If you want a schema-defined interface, delete the model.load
+    # line below and delete the matching file in the models sub-directory.
+    # If you want a model-based interface, edit
+    # models/banner_image.xml to define the content type.
+
+    form.model("models/banner_image.xml")
+
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+
+class BannerImage(Container):
+    grok.implements(IBannerImage)
+
+    # Add your class methods and properties here
+
+
+# View class
+# The view will automatically use a similarly named template in
+# banner_image_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class SampleView(grok.View):
+    """ sample view class """
+
+    grok.context(IBannerImage)
+    grok.require('zope2.View')
+
+    # grok.name('view')
+
+    # Add view methods here
