e867886624333450da165369f8e80213284e77dd
Daniel Jowett <daniel@jowettenterprises.com>
2013-11-14 22:17:41 +0000

    Validate LeadImage dimensions

--- jowent/bannerview/validators.py
+++ jowent/bannerview/validators.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone.namedfile.interfaces import INamedBlobImageField
 # from plone.app.contenttypes.interfaces import IImage
+from plone.app.contenttypes.behaviors.leadimage import ILeadImage
 from zope.interface import Invalid
 from z3c.form import validator
 from five import grok
@@ -8,17 +9,36 @@ from five import grok
 # 1 MB size limit
 MAXSIZE = 1024 * 1024
 
+REQUIRED_HEIGHT = 320
+REQUIRED_WIDTH = 2000
+
 
 class ImageDimensionsValidator(validator.FileUploadValidator):
 
     def validate(self, value):
         super(ImageDimensionsValidator, self).validate(value)
 
-        if value.getSize() > MAXSIZE:
-            raise Invalid("Image is too large (too many bytes)")
+        if value:
+            # See: plone.namedfile.file.NamedBlobImage
+            width, height = value.getImageSize()
+            
+            if REQUIRED_HEIGHT and REQUIRED_WIDTH:
+                if (width != REQUIRED_WIDTH or
+                    height != REQUIRED_HEIGHT):
+                    raise Invalid("Image has wrong dimensions - it should be %d x %d pixels (h x w)"
+                                  ", but is %d x %d" %
+                                  (REQUIRED_HEIGHT, REQUIRED_WIDTH, height, width))
+            elif REQUIRED_HEIGHT:
+                if (height != REQUIRED_HEIGHT):
+                    raise Invalid("Image is the wrong height - it should be %d pixels" %
+                                  (REQUIRED_HEIGHT))
+            elif REQUIRED_WIDTH:
+                if (width != REQUIRED_WIDTH):
+                    raise Invalid("Image is the wrong width - it should be %d pixels" %
+                                  (REQUIRED_WIDTH))
 
 
 validator.WidgetValidatorDiscriminators(ImageDimensionsValidator,
-#                                        context=IImage,
-                                        field=INamedBlobImageField)
+                                        context=ILeadImage,
+                                        field=ILeadImage['image'])
 grok.global_adapter(ImageDimensionsValidator)
