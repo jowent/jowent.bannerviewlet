be12e911f2cda5aa3b85877f2a469b18ece28803
Daniel Jowett <daniel@jowettenterprises.com>
2014-01-08 17:01:09 +0000

    BannerSlide should not be Folderish!   Also tidied imports

--- jowent/bannerview/banner_slide.py
+++ jowent/bannerview/banner_slide.py
@@ -2,15 +2,9 @@ from five import grok
 
 from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from plone.dexterity.content import Container
+from plone.dexterity.content import Item
 from plone.directives import dexterity, form
-from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
 
 
@@ -32,7 +26,7 @@ class IBannerSlide(form.Schema, IImageScaleTraversable):
 # methods and properties. Put methods that are mainly useful for rendering
 # in separate view classes.
 
-class BannerSlide(Container):
+class BannerSlide(Item):
     grok.implements(IBannerSlide)
 
     # Add your class methods and properties here
