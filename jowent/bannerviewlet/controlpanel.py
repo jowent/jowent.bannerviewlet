1c1f548dd7a38dc139b81e734109d0b06595d222
Daniel Jowett <daniel@jowettenterprises.com>
2015-07-07 14:46:28 +0100

    Add a note to the controlpanel description; Style it for nice linebreaks

--- jowent/bannerviewlet/controlpanel.py
+++ jowent/bannerviewlet/controlpanel.py
@@ -8,8 +8,14 @@ class BannerViewletSettingsEditForm(controlpanel.RegistryEditForm):
 
     schema = IBannerViewletSettings
     label = _(u"Banner Viewlet Settings")
-    description = _(u"""These settings control the permitted dimensions and filesize
-    of the Banner Images which are used to fill the BannerViewlet.
+    description = _(u"""    These settings control the permitted dimensions and filesize of the BannerViewlet (and the Banner Images which are used to fill it).
+
+    Note that changing the dimensions settings will resize the BannerViewlet, but it will NOT resize existing Banner Images.
+    Depending on which way you are resizing this could result in:
+        a) smaller images looking blurry when stretched to match dimensions
+        b) blank space to the side of images when height/width ratio changes
+        c) larger images taking more diskspace/memory than they need to
+    These issues can be resolved by simply updating all Banner Images across your site.
     """)
 
     def updateFields(self):
